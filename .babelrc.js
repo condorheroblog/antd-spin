92d2500fd45d4a413cf737bc7440f7171fe8a3a2
Condor Hero <47056890+condorheroblog@users.noreply.github.com>
2020-11-26 17:02:25 +0800

    Update .babelrc.js
    remove @babel/plugin-transform-react-jsx

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,3 @@
 const presets = ["@babel/preset-env", "@babel/preset-react"];
-const plugins = ["@babel/plugin-transform-react-jsx", "@babel/plugin-proposal-class-properties"];
+const plugins = ["@babel/plugin-proposal-class-properties"];
 module.exports = { presets, plugins };
